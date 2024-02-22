0ecbaf4f8ef5b709a1f35a98c255548d97501bb5
Rahul-Alladi <rahul@risksek.com>
2024-02-23 02:52:33 +0530

    used conditions loops inside jinja and displayed dynamic data in the html pages,added a default page

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/',views.projects,name='projects'),
-    path('project/<str:pk>',views.project,name='project')
+    path('',views.projects,name='projects'), # leaving the url path makes it the default page in this case when u open http://127.0.0.1:8000/ it opens projects page
+    path('projectpage/<str:pk>',views.project,name='project')
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+        {
+            'id':'1',
+            'title':'Ecommerce website',
+            'description':'Ecommerce website'
+        },
+        {
+            'id':'2',
+            'title':'Ecommerce website 2',
+            'description':'Ecommerce website 2'
+        },
+        {
+            'id':'3',
+            'title':'Ecommerce website 3',
+            'description':'Ecommerce website 3 '
+        },
+]
+
 # Create your views here.
 def projects(request):
-    return render(request,'projects/projects.html')
+    page = 'projects'
+    number = 9
+    context = {'page':page,'number':number,'projects':projectsList}
+    return render(request,'projects/projects.html', context)#msg if u wanna pass anything dynamic to the page
 
 def project(request,pk):
-    return render(request,'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+
+    return render(request,'projects/single-project.html',{'project':projectObj})
